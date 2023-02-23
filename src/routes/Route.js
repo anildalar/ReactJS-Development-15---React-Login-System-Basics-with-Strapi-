b4357c8bc929b378317bb6e3c80261cff27f472c
unknown <dollorinfotech@gmail.com>
2023-02-23 09:39:06 +0530

    new

---
+++ src/routes/Route.js
@@ -0,0 +1,23 @@
+import Contact from '../pages/Contact'
+import Home from '../pages/Home';
+import Login from '../pages/Login'
+import NotFound from '../pages/NotFound';
+
+export const routes = [ // Array of 
+            { 
+                path:"/",
+                element:<Home/>
+            },
+            { 
+                path:"/login",
+                element:<Login/>
+            },
+            { 
+                path:"/contact",
+                element:<Contact />
+            },
+            { 
+                path:"*",
+                element:<NotFound />
+            }
+        ];
